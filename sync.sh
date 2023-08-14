14d564b3e74d88a021d2b92e910d3dd5e960b66c
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-08-14 14:59:57 +0300

    updates

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/.config/rofi/ . -r
+cp ~/.config/sway/ . -r
+cp ~/.config/swaylock/ . -r
+cp ~/.config/waybar/ . -r
+cp ~/.config/workstyle/ . -r
+cp ~/.config/neofetch/ . -r
+cp ~/.config/kitty/ . -r
